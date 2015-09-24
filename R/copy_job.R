10fa2b40c57a293bcab78b4490f6b5b9ece59186
leeper <thosjleeper@gmail.com>
2015-09-24 14:33:54 +0100

    add copy job func

---
+++ R/copy_job.R
@@ -0,0 +1,41 @@
+#' @rdname copyJob
+#' @export
+#'
+#' @title 
+#' Copy a job and, optionally, its data to a new job ID.
+#'
+#' @description
+#' \code{copyJob} copies an existing job to a new job ID.
+#'
+#' @param id A character string containing an ID for job.
+#'
+#' @param rows A logical indicating whether to copy rows from the original job.
+#'
+#' @param verbose A logical indicating whether to print additional information about the request.
+#'
+#' @param ... Additional arguments passed to \code{\link{APIcall}}.
+#'
+#' @return A character string contianing the job ID, invisibly.
+#' 
+#' @examples
+#' \dontrun{
+#' copyJob(id = 'jobid')
+#' }
+#'
+#' @seealso \code{\link{createJob}}, \code{\link{updateJob}}
+
+
+getStatus <- function(id, rows = TRUE, verbose = TRUE, ...){
+
+	# API request
+	endpoint <- paste0('jobs/', id, '/copy.json')
+	if (rows) {
+		newjob <- APIcall(endpoint, params = "&all_units=true", ...)
+	} else {
+		newjob <- APIcall(endpoint, ...)
+	}
+
+	if (verbose) message("Job successfully created with ID = ", newjob$id)
+
+	return(invisible(newjob$id))
+}
