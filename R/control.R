a9526e33e63760d7c4fad7150617358c87ac2d41
Pablo Barberá <pablo.barbera@nyu.edu>
2015-10-01 10:48:36 -0400

    added launchJob function

--- R/control.R
+++ R/control.R
@@ -1,3 +1,52 @@
+#' @rdname launchJob
+#' @export
+#'
+#' @title 
+#' Pause a job
+#'
+#' @description
+#' \code{launchJob} launches a new job.
+#'
+#' @param id A character string containing an ID for job.
+#'
+#' @param channel Either "on_demand" (for On Demand worforce), "cf_internal"
+#' (for the Internal Channel) or "both"
+#'
+#' @param units Units to be launched.
+#'
+#' @param ... Additional arguments passed to \code{\link{APIcall}}.
+#'
+#' @return A character string containing the job ID, invisibly.
+#' 
+#' @examples
+#' \dontrun{
+#' launchJob('jobid')
+#' }
+#'
+#' @seealso \code{\link{pauseJob}}, \code{\link{cancelJob}}, \code{\link{resumeJob}}
+
+launchJob <- function(id, channel="cf_internal", units=100, ...){
+	
+	# preparing body of request
+	body <- list()
+	if (channel=="on_demand") body['channels[0]'] <- "on_demand"
+	if (channel=="cf_internal") body['channels[0]'] <- "cf_internal"
+	if (channel=="both"){
+		body['channels[0]'] <- "on_demand"
+		body['channels[1]'] <- "cf_internal"
+	}
+	if (length(body)==0) stop("Wrong channel option?")
+	body['debit[units_count]'] <- units
+
+
+	# API request
+	endpoint <- paste0('jobs/', id, '/orders.json')
+	jobinfo <- APIcall(endpoint, type="POST", body=body, ...)
+
+	if (verbose) message(sprintf("Job %s successfully launched", id))
+	return(invisible(jobinfo$id))
+}
+
 #' @rdname pauseJob
 #' @export
 #'
@@ -18,7 +67,7 @@
 #' pauseJob('jobid')
 #' }
 #'
-#' @seealso \code{\link{cancelJob}}, \code{\link{resumeJob}}
+#' @seealso \code{\link{launchJob}}, \code{\link{cancelJob}}, \code{\link{resumeJob}}
 
 pauseJob <- function(id, ...){
 	# API request
@@ -49,7 +98,7 @@ pauseJob <- function(id, ...){
 #' cancelJob('jobid')
 #' }
 #'
-#' @seealso \code{\link{pauseJob}}, \code{\link{resumeJob}}
+#' @seealso \code{\link{launchJob}}, \code{\link{pauseJob}}, \code{\link{resumeJob}}
 
 cancelJob <- function(id, ...){
 	# API request
@@ -80,7 +129,7 @@ cancelJob <- function(id, ...){
 #' getStatus('jobid')
 #' }
 #'
-#' @seealso \code{\link{cancelJob}}, \code{\link{pauseJob}}
+#' @seealso \code{\link{launchJob}}, \code{\link{cancelJob}}, \code{\link{pauseJob}}
 
 resumeJob <- function(id, ...){
 	# API request
