6eb0038a699b78301f455b3f8c0dea5bb2d3bde5
Pablo Barberá <pablo.barbera@nyu.edu>
2015-10-01 10:48:45 -0400

    new settings in updateJob

--- R/update_job.R
+++ R/update_job.R
@@ -12,6 +12,10 @@
 #'
 #' @param cml New layout for job.
 #'
+#' @param payment_cents Amount, in cents, that contributors will be paid per task.
+#'
+#' @param units_per_assignment Number of rows that will comprise one page within a job.
+#'
 #' @param ... Additional arguments passed to \code{\link{APIcall}}.
 #'
 #' @return A character string containing the job ID, invisibly.
@@ -24,13 +28,16 @@
 #'
 #' @seealso \code{\link{createJob}}
 
-updateJob <- function(id, title=NULL, instructions=NULL, cml=NULL, ...){
+updateJob <- function(id, title=NULL, instructions=NULL, cml=NULL, 
+	payment_cents=NULL, units_per_assignment=NULL, ...){
 
 	# preparing body of request
 	body <- list()
 	if (!is.null(title)) body['job[title]'] <- title
 	if (!is.null(instructions)) body['job[instructions]'] <- instructions
 	if (!is.null(cml)) body['job[cml]'] <- cml
+	if (!is.null(payment_cents)) body['job[payment_cents]'] <- payment_cents
+	if (!is.null(units_per_assignment)) body['job[units_per_assignment]'] <- units_per_assignment
 
 	# API request to create job
 	endpoint <- paste0('jobs/', id, '.json')
