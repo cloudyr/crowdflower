957851e86b6cbb58ec726f67842e2bdf529fac0d
Thomas J. Leeper <thosjleeper@gmail.com>
2016-01-29 13:14:02 +0000

    rewrite API query workhorse func

--- R/auth.R
+++ R/auth.R
@@ -10,7 +10,7 @@
 #'
 #' @param endpoint endpoint of API request.
 #'
-#' @param params additional text to add to request URL.
+#' @param query A list containing named URL query parameters to add to request URL.
 #'
 #' @param body additional parameters to add to request body.
 #'
@@ -20,39 +20,46 @@
 #'
 #' @param base_url Base URL of API request.
 #'
-#' @return A character string containing the JSON response from the Crowdflower API.
+#' @param ... Additional arguments passed to an HTTP request function, e.g, \code{\link[httr]{GET}} or \code{\link[httr]{POST}}.
+#'
+#' @return An integer specifying the new Crowdflower job ID.
 
-APIcall <- function(endpoint, params=NULL, body=NULL, type="GET", 
+APIcall <- function(endpoint, 
+                    query = NULL, 
+                    body = NULL, 
+                    type = "GET", 
                     key = Sys.getenv("CROWDFLOWER_API_KEY"),
-                    base_url='https://api.crowdflower.com/v1/'){
+                    base_url = 'https://api.crowdflower.com/v1/', 
+                    ...){
 
-	# params for API call
-	url <- paste0(base_url, endpoint, '?key=', key)
-	if (!is.null(params)) url <- paste0(url, params)
-	
-	# making API request
-	if (type=="GET") r <- httr::GET(url)
-	if (type=="POST") r <- httr::POST(url, body=body)
-	if (type=="POST-DATA") r <- httr::POST(url, body=body, encode="json")
-	if (type=="PUT") r <- httr::PUT(url, body=body)
+    # params for API call
+    query <- c(list(key = key), query)
+    url <- paste0(base_url, endpoint)
+    
+    # making API request
+    r <- switch(type, 
+                GET = httr::GET(url, query = query, ...),
+                POST = httr::POST(url, query = query, ..., body = body),
+                "POST-DATA" = httr::POST(url, query = query, ..., body = body, encode="json"),
+                PUT = httr::PUT(url, query = query, ..., body = body),
+                stop("Value of request 'type' unrecognized."))
+    httr::stop_for_status(r)
+    
+    # trying to parse response
+    error <- tryCatch(httr::content(r),
+                      error = function(e) e)
+    if (!inherits(error, 'error')){
+        # if JSON contains embedded error message, return it
+        if (length(error$error)){
+            stop(error$error)
+        }
+        # else return the response value
+        response <- error
+    } else {
+        # try returning raw text response
+        warning("Response was not JSON")
+        response <- httr::content(r, 'text')
+    }
 
-	# trying to parse response
-	error <- tryCatch(response <- httr::content(r),
-		error = function(e) e)
-	# if response contains error message, return that message
-	if (!inherits(error, 'error')){
-		if (length(response$error)>0){
-			stop(response$error$message)
-		}
-	}
-	# if content was not properly parsed with default (json),
-	# try raw text
-	if (inherits(error, 'error')){
-		response <- httr::content(r, 'text')
-	}
-
-	return(response)
-
+    return(response)
 }
-
-
