2855d92b467e96f93ec6fe431ddeeb4867e86544
Thomas J. Leeper <thosjleeper@gmail.com>
2016-06-28 15:03:04 +0100

    fix duplicated func page

--- R/get_account.R
+++ R/get_account.R
@@ -1,23 +1,16 @@
 #' @rdname getAccount
-#' @export
-#'
-#' @title 
-#' Retrieve account information
-#'
+#' @title Retrieve account information
 #' @description
 #' \code{getAccount} retrieves the CrowdFlower account information
 #' related to the API key for the authenticated user
-#'
 #' @param ... Additional arguments passed to \code{\link{crowdflowerAPIQuery}}.
-#'
 #' @return A list containing account details for the authenticated user.
-#'
 #' @references \href{https://success.crowdflower.com/hc/en-us/articles/202703425-CrowdFlower-API-Requests-Guide#account_information}{Crowdflower API documentation}
-#' 
 #' @examples
 #' \dontrun{
 #' getAccount()
 #' }
+#' @export
 
 getAccount <- function(...){
 
